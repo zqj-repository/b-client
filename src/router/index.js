60723e7460b663ae9add28be29d6fc4db11dc7bb
Jack Zhang <jack.zhang@ehealth.com>
2020-01-06 17:03:02 +0800

    init commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,52 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Articles from '@/pages/Articles'
 
 Vue.use(Router)
 
+/**
+ * /
+ * article list
+ * article details
+ * /admin
+ * article poster
+ * category
+ */
+
 export default new Router({
+  mode: 'history',
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+			path: '/',
+			name: 'Home',
+			redirect: '/articles'
+		},
+    {
+      path: '/articles',
+      name: 'Articles',
+      component: Articles
+    },
+    {
+      path: '/article-detail',
+      name: 'Article Detail',
+      component: ArticleDetail
+    },
+    {
+      path: '/admin',
+      name: 'Admin Home',
+      component: Admin,
+      children: [
+        {
+          path: 'article-management',
+          name: 'Article Management',
+          component: ArticleManagement
+        },
+        {
+          path: 'category-management',
+          name: 'Category Management',
+          component: CategoryManagement
+        }
+      ]
     }
   ]
 })
